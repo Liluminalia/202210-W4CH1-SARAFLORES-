0c806fbe5c259ed8297693d21a218c76678b97be
Liluminalia <peloazul4@gmail.com>
2022-10-24 21:17:34 +0200

    trying dynamic

--- src/Gentleman.tsx
+++ src/Gentleman.tsx
@@ -1,41 +1,45 @@
+import { PointingMenInfo } from "./models/data";
+
 export function Gentleman() {
-    return (
+    return PointingMenInfo.forEach((i: any) => {
         <>
             <li className="gentleman">
                 <div className="gentleman__avatar-container">
                     <img
                         className="gentleman__avatar"
-                        src="img/fary.jpg"
-                        alt="The Fary pointing at you"
+                        src={"img/" + PointingMenInfo[i].picture}
+                        alt={PointingMenInfo[i].alternativeText}
                     />
                     <span className="gentleman__initial">F</span>
                 </div>
                 <div className="gentleman__data-container">
-                    <h2 className="gentleman__name">The Fary</h2>
+                    <h2 className="gentleman__name">
+                        {PointingMenInfo[i].name}
+                    </h2>
                     <ul className="gentleman__data-list">
                         <li className="gentleman__data">
                             <span className="gentleman__data-label">
                                 Profession:
                             </span>
-                            Influencer
+                            {PointingMenInfo[i].profession}
                         </li>
                         <li className="gentleman__data">
                             <span className="gentleman__data-label">
                                 Status:
                             </span>{" "}
-                            RIP
+                            {PointingMenInfo[i].status}
                         </li>
                         <li className="gentleman__data">
                             <span className="gentleman__data-label">
                                 Twitter:
                             </span>{" "}
-                            Pending
+                            {PointingMenInfo[i].twitter}
                         </li>
                     </ul>
                 </div>
                 <i className="icon gentleman__icon fas fa-check"></i>
                 <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
             </li>
-        </>
-    );
+        </>;
+    });
 }
