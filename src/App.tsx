0f7bb1b35a2fa00d773f71135da0d34bf0ae1c83
Liluminalia <peloazul4@gmail.com>
2022-10-24 19:55:15 +0200

    component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./Header";
 
 function App() {
     return (
         <div className="container">
-            <header className="main-header">
-                <h1 className="main-title">The pointing gentlemen</h1>
-            </header>
-            <section className="controls">
-                <p className="info">0 gentlemen pointing at you</p>
-                <button className="button button--select">Select all</button>
-            </section>
+            <Header></Header>
+
             <main className="main">
                 <ul className="gentlemen">
                     <li className="gentleman">
